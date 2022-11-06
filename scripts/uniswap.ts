636ad4c38d6347ee73169dee902540c112add9ad
urlauba <austinurlauba@gmail.com>
2022-11-05 23:47:48 -0700

    uniswap spot price example USDC/WETH

---
+++ scripts/uniswap.ts
@@ -0,0 +1,104 @@
+import { ethers } from 'ethers'
+import { Pool } from '@uniswap/v3-sdk'
+import { Token } from '@uniswap/sdk-core'
+import { abi as IUniswapv3PoolABI } from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_NODE_URL)
+const poolAddress = '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8' // USDC over WETH
+const poolContract = new ethers.Contract(poolAddress, IUniswapv3PoolABI, provider)
+
+interface Immutables {
+    factory: string
+    token0: string
+    token1: string
+    fee: number
+    tickSpacing: number
+    maxLiquidityPerTick: ethers.BigNumber
+}
+
+interface State {
+    liquidity: ethers.BigNumber
+    sqrtPriceX96: ethers.BigNumber
+    tick: number
+    observationIndex: number
+    observationCardinality: number
+    observationCardinalityNext: number
+    feeProtocol: number
+    unlocked: boolean
+}
+
+async function getPoolImmutables() {
+    const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] = await Promise.all([
+        poolContract.factory(),
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+        poolContract.tickSpacing(),
+        poolContract.maxLiquidityPerTick(),
+    ])
+
+    const immutables: Immutables = {
+        factory,
+        token0,
+        token1,
+        fee,
+        tickSpacing,
+        maxLiquidityPerTick,
+    }
+
+    return immutables
+}
+
+async function getPoolState() {
+    const [liquidity, slot] = await Promise.all([poolContract.liquidity(), poolContract.slot0()])
+
+    const PoolState: State = {
+        liquidity,
+        sqrtPriceX96: slot[0],
+        tick: slot[1],
+        observationIndex: slot[2],
+        observationCardinality: slot[3],
+        observationCardinalityNext: slot[4],
+        feeProtocol: slot[5],
+        unlocked: slot[6],
+    }
+
+    return PoolState
+}
+
+async function main() {
+    const [immutables, state] = await Promise.all([getPoolImmutables(), getPoolState()])
+
+    const TokenA = new Token(3, immutables.token0, 6, 'USDC', 'USD Coin')
+    const TokenB = new Token(3, immutables.token1, 18, 'WETH', 'Wrapped Ether')
+
+    const pool = new Pool(
+        TokenA,
+        TokenB,
+        immutables.fee,
+        state.sqrtPriceX96.toString(),
+        state.liquidity.toString(),
+        state.tick
+        // (optional) tick data - can be used to model the result of a swap
+    )
+
+    const token0Price = pool.token0Price
+    const token1Price = pool.token1Price
+
+    // token0 is USDC
+    // 1 USDC = 0.00062 WETH
+    // token0 significant price is 0.00061866
+    // if I want 1 of token0(USDC), I need 0.00061866 of token1(WETH)
+    console.log(token0Price.toSignificant(6))
+
+    // token1 is WETH
+    // 1 WETH = 1,616.77 USDC
+    // token1 significant price is 1617.69
+    // if I want 1 of token1(WETH), I need 1617.69 of token0(USDC)
+    console.log(token1Price.toSignificant(6))
+}
+
+main()
